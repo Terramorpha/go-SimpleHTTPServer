9720c73769cc01b85278eb99dc358990d5cff0b8
terramorpha <Justun1011@gmail.com>
2018-10-29 22:01:26 -0400

    removes leaky files

--- WebUI/main.js
+++
@@ -1,6 +0,0 @@
-var req = new XMLHttpRequest();
-req.onreadystatechange = function() {
-    console.log(req.responseText)
-}
-xmlHttp.open( "GET", "/settings.json", true ); // false for synchronous request
-xmlHttp.send( null );
